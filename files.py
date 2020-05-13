db72958463ba7579b7d9ffb1a17a240ba4e1ed5d
marcomoauro <marco@wanderio.com>
2020-05-13 14:58:56 +0200

    update script

--- files.py
+++ files.py
@@ -2,18 +2,35 @@ import random
 
 source = 'path/to/historical_stock_prices.csv'
 
-percs = ['0.25', '0.50', '0.75']
 
-for perc in percs:
-    destination = source.split('.')[0] + '-' + perc.split('.')[1] + '.csv'
-    rows = []
-    with open(source, 'r') as f:
-        lines = f.readlines()
-        rows.append(lines[0])
-        for line in lines[1:]:
-            if random.uniform(0, 1) < float(perc):
-                rows.append(line)
+def files(bool):
+    percs = ['0.25', '0.50', '0.75']
+    for perc in percs:
+        destination = source.split('.')[0] + '-' + name(bool, perc) + '.csv'
+        rows = []
+        new_rows = []
+        with open(source, 'r') as f:
+            lines = f.readlines()
+            rows.append(lines[0])
+            for line in lines[1:]:
+                if bool:
+                    fields = line.strip().split(',')
+                    fields[0] = 'MR' + fields[0]
+                    fields = ','.join(fields)
+                    new_rows.append(fields)
+                if random.uniform(0, 1) < float(perc):
+                    rows.append(line)
 
-    with open(destination, 'a') as f:
-        for line in rows:
-            f.write(line)
+        with open(destination, 'a') as f:
+            for line in rows:
+                f.write(line)
+            for line in new_rows:
+                f.write(line + '\n')
+
+
+def name(flag, percentage):
+    return '1' + percentage.split('.')[1] if flag else percentage.split('.')[1]
+
+
+files(True)
+files(False)
