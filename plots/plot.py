ee27eb1c320b2551548b3f42daaa1fa9c43bc0f4
marcomoauro <marco@wanderio.com>
2020-05-16 14:50:38 +0200

    plot

---
+++ plots/plot.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from plots.data import DATA
+
+
+def plot(map_reduce_values, spark_values, hive_values, job, env):
+    ind = np.arange(10)
+    width = 0.26
+    plt.bar(ind, map_reduce_values, width, label='MapReduce')
+    plt.bar(ind + width, spark_values, width, label='Spark')
+    plt.bar(ind + (width * 2), hive_values, width, label='Hive')
+
+    plt.title(env.title() + ' - ' + job.title())
+    plt.ylabel('Time (S)')
+
+    plt.xticks(ind + width, ('25%', '50%', '75%', '100%', '125%', '150%', '175%', '200%', '400%', '800%'))
+    plt.legend(loc='best')
+    plt.show()
+
+
+for env in DATA.keys():
+    for job in DATA['local']['map_reduce'].keys():
+        plot(DATA[env]['map_reduce'][job], DATA[env]['spark'][job], DATA[env]['hive'][job], job, env)
